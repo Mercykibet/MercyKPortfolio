142e1f8718134d29a2152fbb1d7995584c44f984
mercy kibet <kibetmercy9@gmail.com>
2025-04-18 22:29:02 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import { useState } from 'react'
-import React from 'react'
+import { useState } from "react";
+import React from "react";
 
-import Navbar from './components/navbar/Navbar'
-import About from './components/about/About'
-import WorkExperience from './components/portfolio/WorkExperience'
-import Education from './components/portfolio/Education'
-import Projects from './components/portfolio/Projects'
-import Skills from './components/portfolio/Skills'
-import Footer from './components/footer/Footer'
-import Contact from './components/contact/Contact'
-
+import Navbar from "./components/navbar/Navbar";
+import About from "./components/about/About";
+import WorkExperience from "./components/portfolio/WorkExperience";
+import Education from "./components/portfolio/Education";
+import Projects from "./components/portfolio/Projects";
+import Skills from "./components/portfolio/Skills";
+import Footer from "./components/footer/Footer";
+import Contact from "./components/contact/Contact";
 
 function App() {
- 
   return (
-    <>              
-   <Navbar/>
-   <About/>
-   <WorkExperience/>
-   <Education/>
-   <Projects/>
-   <Skills/>
-  <Contact/>
-   <Footer/>
+    <>
+      <Navbar />
+      <About />
+      <WorkExperience />
+      <Education />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
     </>
-   
-  )
+  );
 }
 
-export default App
+export default App;
